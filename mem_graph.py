38b5df036b528b933232f756b94d79844b832248
David Baugher <davidbaugher@Davids-MBP.attlocal.net>
2018-06-12 10:48:13 -0500

    added file for graph based memory

---
+++ mem_graph.py
@@ -0,0 +1,10 @@
+####  Graph based memory for mapping continuous spaces
+
+# need to: build function for finding similar graph nodes, create threshold parameter for 
+#          creating new nodes, create node structure
+
+# node: value, links, link weights
+ 
+# is this new value between one of our nodes? If so, strengthen weight between those nodes;
+#  if not, add to graph. 
+
